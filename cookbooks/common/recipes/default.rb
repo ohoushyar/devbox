b36c302f8c8855d422ecd0e9732012ddc79ccc2e
Omid Houshyar <ohoushyar@gmail.com>
2013-02-20 11:51:32 +0800

    Added few perl modules

--- cookbooks/common/recipes/default.rb
+++ cookbooks/common/recipes/default.rb
@@ -19,5 +19,18 @@ package 'libssl-dev' # used by Net::Twitter
 cpan_module 'YAML'
 cpan_module 'Params::Validate'
 cpan_module 'DateTime'
+cpan_module 'Data::Printer'
 cpan_module 'DDP'
+
+cpan_module 'Test::More'
+cpan_module 'Test::Most'
+cpan_module 'Test::Exception'
+
+cpan_module 'Moose'
+cpan_module 'Moo'
+cpan_module 'Mo'
+
+cpan_module 'File::Next'
+
+
 
